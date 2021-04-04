324c6f516ff38643b84248807e42f474677989cf
Wact B. Prot <wactbprot@gmail.com>
2021-04-04 18:17:54 +0200

    trans pad

--- components/stmem/src/metis/stmem/trans.clj
+++ components/stmem/src/metis/stmem/trans.clj
@@ -11,9 +11,11 @@
   May be:
   * `tasks`
   * `<mp-id>`"
-  [k]
+  ([k]
+   (key->mp-id c/config k))
+  ([k c]
   (when (and (string? k) (not (empty? k)))
-    (nth (string/split k re-sep) 0 nil)))
+    (nth (string/split k (:re-sep c)) 0 nil))))
 
 ;;------------------------------
 ;; key at position 1
@@ -28,10 +30,11 @@
   * `container`
   * `exchange`
   * `id`
-  * `meta`
-  "
-  [k]
-  (when (string? k) (nth (string/split k re-sep) 1 nil)))
+  * `meta`"
+  ([k]
+   (key->struct c/config k))
+  ([k c]
+  (when (string? k) (nth (string/split k (:re-sep c)) 1 nil))))
 
 ;;------------------------------
 ;; key at position 2
@@ -39,8 +42,10 @@
 (defn key->no-idx
   "Returns the value of the key corresponding to the given key
   `container` or `definitions` index."
-  [k]
-  (when (string? k) (nth (string/split k re-sep) 2 nil)))
+  ([k]
+   (key->no-idx c/config k))
+  ([k c]
+  (when (string? k) (nth (string/split k (:re-sep c)) 2 nil))))
 
 ;;------------------------------
 ;; key at position 3
@@ -58,23 +63,27 @@
   * `title`
   * `definition`
   * `messsage`"
-  [k]
-  (when (string? k) (nth (string/split k re-sep) 3 nil)))
+  ([k]
+   (key->func c/config k))
+  ([k c]
+  (when (string? k) (nth (string/split k (:re-sep c)) 3 nil))))
 
 ;;------------------------------
 ;; key at position 4
 ;;------------------------------
 (defn key->seq-idx
   "Returns an integer corresponding to the givens key sequential index."
-  [k]
-  (when (string? k) (nth (string/split k re-sep) 4 nil)))
+  ([k]
+   (key->seq-idx c/config k))
+  ([k c]
+   (when (string? k) (nth (string/split k (:re-sep c)) 4 nil))))
 
 (defn key->no-jdx
   "The 4th position at `definitions` keys."
   [k]
   (key->seq-idx k))
 
-(defn key->level
+  (defn key->level
   "The 4th position at listener `reg` keys."
   [k]
   (key->seq-idx k))
@@ -84,23 +93,57 @@
 ;;------------------------------
 (defn key->par-idx
   "Returns an integer corresponding to the givens key parallel index."
-  [k]
-  (when (string? k) (nth (string/split k re-sep) 5 nil)))
+  ([k]
+   (key->par-idx c/config k))
+  ([k c]
+   (when (string? k) (nth (string/split k (:re-sep c)) 5 nil))))
+
+;;------------------------------
+;; map to string
+;;------------------------------
+(defn pad-ok?
+  "Checks if the padding of `i` is ok. `\"*\"` serves pattern matching."
+  ([i]
+   (pad-ok? i (:stmem-key-pad-length c/config)))
+  ([i n]
+   (cond
+     (and (string? i)
+          (= n (count i))) true
+     (= i "*")             true 
+     :else                 false)))
+
+(defn ensure-int
+  "Ensures `i` to be integer. Returns 0 as default."
+  [i]
+  (if (integer? i) i (try (Integer/parseInt i) (catch Exception e 0))))
+
+(defn lpad
+  "Left pad the given number if it is not a string."
+  ([i]
+   (lpad i (:stmem-key-pad-length c/config)))
+  ([i n]
+   (if (pad-ok? i) i (format (str "%0" n "d") (ensure-int i)))))
+
+(defn map->task-key
+  [{{t :tasks} :stmem-trans s :stmem-key-sep} {n :task-name}]
+  (str n s t))
+
+(defn map->struct-key
+  [{trans :stmem-trans s :stmem-key-sep} m]
+  (str (:mp-id m) s ((:struct m) trans)))
 
 (defn map->key
   ([m]
    (map->key c/config m))
   ([config m]
-   (when (and (map? m) (not (empty m))) 
-     (let [s (:stmem-key-sep config)]
-       (str (if (:task-name m) "tasks" (:mp-id m)) s
-            (when (:struct m)
-              (str s (:struct m))
-              (when (:no-idx m)
-                (str s (:no-idx m))
-                (when (:func m)
-                  (str s (:func m))
-                  (when (:seq-idx m)
-                    (str s (:seq-idx m))
-                    (when (:par-idx m)
-                      (str s (:par-idx m))))))))))))
+   (when (and (map? m) (not (empty? m))) 
+     (let [sep   (:stmem-key-sep config)
+           trans (:stmem-trans config)]
+       (if (:task-name m)
+         (map->task-key config m)
+         (when (and (:mp-id m) (:struct m))
+           (str (map->struct-key config m))
+           (when (:no-idx m) (str sep (:no-idx m))
+                  (when (:func m) (str sep (:func m))
+                        (when (:seq-idx m) (str sep (:seq-idx m))
+                              (when (:par-idx m) (str sep (:par-idx m))))))))))))
--- components/stmem/test/metis/stmem/trans_test.clj
+++ components/stmem/test/metis/stmem/trans_test.clj
@@ -7,3 +7,41 @@
     (is (nil? (trans/map->key nil))))
   (testing "empty"
     (is (nil? (trans/map->key {})))))
+
+(deftest lpad-i
+  "  
+  Example:
+  ```clojure
+   (u/lp 2)
+  ;; \"002\"
+  (u/lp \"02\")
+  ;; \"002\"
+  (u/lp 2)
+  ;; \"002\"
+  (u/lp true)
+  ;; \"000\"
+  (u/lp \"003\")
+  ;; \"003\"
+  (u/lp \"000003\")
+  ;; \"003\"
+  ```"
+  (testing "nil behaviour"
+    (is (nil? nil))))
+
+(deftest lpad-i
+  "  
+  Example:
+  ```clojure
+  (u/ensure-int 100)
+  ;; 100
+  (u/ensure-int \"w\")
+  ;; 0
+  (u/ensure-int \"00\")
+  ;; 0
+  (u/ensure-int \"10\")
+  ;; 10
+  (u/ensure-int true)
+  ;; 0
+  ```"
+  (testing "nil behaviour"
+    (is (nil? nil))))
