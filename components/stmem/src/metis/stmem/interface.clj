6d579d7439b6326cbcb529f7426b1ae3e2d20eaf
Wact B. Prot <wactbprot@gmail.com>
2021-04-08 09:51:00 +0200

    notif interface

--- components/stmem/src/metis/stmem/interface.clj
+++ components/stmem/src/metis/stmem/interface.clj
@@ -1,5 +1,6 @@
 (ns metis.stmem.interface
-  (:require [metis.stmem.trans :as trans]))
+  (:require [metis.stmem.trans :as trans]
+            [metis.stmem.notif :as notif]))
 
 (defn set-val [m] (trans/set-val m))
 
@@ -8,3 +9,9 @@
 (defn del-val [m] (trans/del-val m))
 
 (defn del-vals [m] (trans/del-vals m))
+
+(defn register [m f] (notif/register m f))
+
+(defn de-register [m] (notif/de-register m))
+
+(defn clean-register [m] (notif/clean-register m))
