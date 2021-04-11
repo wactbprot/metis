027f44d106192d96d75fa072ad854f99a143e9ec
Wact B. Prot <wactbprot@gmail.com>
2021-04-11 10:43:32 +0200

    clean up notif tests

--- components/stmem/test/metis/stmem/notif_test.clj
+++ components/stmem/test/metis/stmem/notif_test.clj
@@ -35,37 +35,31 @@
                                    :b "b"
                                    :c "c"}} {:mp-id "a" :struct :b :no-idx 5 :func :c})))))
 
-(def reg-test (atom nil))
-
 (deftest register-de-register-i
   (testing "re-de-reg works"
-    (let [x (rand-int 1000)
-          _ (reset! reg-test x)
-          m {:mp-id "test" :struct :cont :no-idx 0 :func :state :seq-idx 0 :value x}
-          f (fn [v] (swap! reg-test dec))]
-      (is (= x  @reg-test))
+    (let [r (atom (rand-int 1000))
+          x        @r
+          m        {:mp-id "test" :struct :cont :no-idx 0 :func :state :seq-idx 0 :value x}
+          f (fn [v] (swap! r dec))]
+      (is (= x  @r))
       (register (assoc m :seq-idx :*) f)
       ;; (Thread/sleep 1)
-      (is (= x @reg-test)
-          "register event triggers but callback is not executed")
-      (trans/set-val (assoc m :value (dec @reg-test)))
-      ;; (Thread/sleep 1)
+      (is (= x @r)
+          "The register event triggers but callback is not executed.")
+      (trans/set-val (assoc m :value (dec @r)))
       (is (= (dec x)
-             @reg-test))
-      (trans/set-val (assoc m :value (dec @reg-test)))
-      ;; (Thread/sleep 1)
+             @r))
+      (trans/set-val (assoc m :value (dec @r)))
       (is (= (dec (dec x))
-             @reg-test))
-      (trans/set-val (assoc m :value (dec @reg-test)))
-      ;; (Thread/sleep 1)
+             @r))
+      (trans/set-val (assoc m :value (dec @r)))
       (is (= (dec (dec (dec x)))
-             @reg-test))
-      (trans/set-val (assoc m :value (dec @reg-test)))
-      ;; (Thread/sleep 1)
+             @r))
+      (trans/set-val (assoc m :value (dec @r)))
       (is (= (dec (dec (dec (dec  x))))
-             @reg-test))
+             @r))
       (de-register (assoc m :seq-idx :*))
-      (trans/set-val (assoc m :value (dec @reg-test)))
-      ;; (Thread/sleep 1)
+      (trans/set-val (assoc m :value (dec @r)))
       (is (= (dec (dec (dec (dec  x))))
-             @reg-test)))))
+             @r)
+          "The set-val function does not trigger after de-register."))))
