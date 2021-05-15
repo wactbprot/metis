497a0e01b0f10b60f5dfc3b0e7e898915e11cc7f
wactbprot <wactbprot@users.noreply.github.com>
2021-05-15 19:40:50 +0000

    deploy: 7df3c3268ec34f62281d8061d0f778d0701a4777

---
+++ metis/document/interface.clj
@@ -0,0 +1,6 @@
+✔ (ns metis.document.interface
+?   (:require [metis.document.core :as core]))
+  
+~ (defn add [m id] (core/add m id))
+  
+  
---
+++ metis/exchange/core.clj
@@ -0,0 +1,105 @@
+✔ (ns metis.exchange.core
+?   ^{:author "wactbprot"
+?     :doc "Handles the access to the exchange interface."}
+?   (:require [com.brunobonacci.mulog :as mu]
+?             [clojure.string :as string]
+?             [metis.utils.interface :as utils]))
+  
+✔ (defn path->second-kw
+?   "Returns the keyword or nil.
+  
+?   ```clojure
+?   (key->second-kw \"foo\" )
+?   ;; nil
+?   (key->second-kw \"foo.bar\" )
+?   ;; :bar
+?   ```"  
+?   [s]
+✔   (when (string? s)
+✔     (when-let [x (second (string/split s #"\."))] (keyword x))))
+  
+✔ (defn path->first-string
+?   "Returns the keyword or nil.
+  
+?   ```clojure
+?   (key->second-kw \"foo\" )
+?   ;; nil
+?   (key->second-kw \"foo.bar\" )
+?   ;; :bar
+?   ```"  
+?   [s]
+✔   (when (string? s)
+✔     (first (string/split s #"\."))))
+  
+✔ (defn get-val [a p]
+✔   (or (get a p)
+✔       (when-let [kw (path->second-kw p)]
+✔         (kw (get a (path->first-string p))))))
+  
+✔ (defn ok?
+?   "Checks a certain exchange endpoint to evaluate to true"
+?   [a p]
+✔   (contains? utils/ok-set (get-val a p)))
+  
+✔ (defn exists? [a p] (some? (get-val a p)))
+  
+✔ (defn stop-if
+?   "Checks if the exchange path given with `:StopIf` evaluates to true."
+?   [a {p :StopIf}]
+✔   (if p
+✔     (ok? a p)
+?     true))
+  
+✔ (defn run-if
+?   "Checks if the exchange path given with `:RunIf` evaluates to true."
+?   [a {p :RunIf}]
+✔   (if p
+✔     (ok? a p)
+?     true))
+  
+✔ (defn only-if-not
+?   "Runs the task `only-if-not` the exchange path given with `:OnlyIfNot`
+?   evaluates to true."
+?   [a {p :OnlyIfNot}]
+~   (cond
+✔     (nil? p) true
+✔     (not (exists? a p)) false
+✔     (not (ok? a p)) true
+✔     (ok? a p) false))
+  
+✔ (defn enclose-map
+?   "Encloses the given map `m` with respect to the key `k`.
+  
+?   Example:
+?   ```clojure
+?   (enclose-map {:gg \"ff\"} \"mm.ll\")
+?   ;; gives:
+?   ;; {\"mm\" {:ll {:gg \"ff\"}}}
+  
+?   (enclose-map {:gg \"ff\"} \"mm\")
+?   ;; gives:
+?   ;; {\"mm\" {:gg \"ff\"}}
+  
+?   (enclose-map {\"mm\" \"ff\"} nil)
+?   ;; gives:
+?   ;; {\"mm\" \"ff\"}
+?   ```"
+?   [m p]
+✔   (if p
+✔     (let [a (path->first-string p)]
+✔       (if-let [b (path->second-kw p)]
+✔         {a {b m}}
+✔         {a m}))
+✔     m))
+  
+✔ (defn fit-in [o m]
+✔   (if o
+✔     (if (and (map? o) (map? m))
+✔       (merge o m)
+✔       m)
+✔     m))
+  
+✔ (defn to-vec [a {x :value p :no-idx  :as m}]
+✔   (into [] (map
+✔             (fn [[k v]] (assoc m :no-idx (name k) :value (fit-in (get a k) v)))
+✔             (enclose-map x p))))
