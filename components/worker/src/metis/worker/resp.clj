7db694a2c255efc5c478f20b0723d4a219cb807f
Wact B. Prot <wactbprot@gmail.com>
2021-05-16 13:05:07 +0200

    resp worker

--- components/worker/src/metis/worker/resp.clj
+++ components/worker/src/metis/worker/resp.clj
@@ -4,11 +4,12 @@
   (:require [cheshire.core :as che]
             [metis.config.interface :as c]
             [metis.exchange.interface :as exch]
+            [metis.document.interface :as doc]
             [com.brunobonacci.mulog :as µ]
             [metis.stmem.interface :as stmem]
             [clojure.string :as string]
             [metis.utils.interface :as u]))
-(comment
+
 (defn do-retry
   ([m]
    (do-retry c/config m))
@@ -25,8 +26,6 @@
          (stmem/set-val (assoc m :value (inc n)))
          {:ok "retry"})))))
 
-
-
 (defn dispatch
   "Dispatches responds from outer space. Expected responses are:
   
@@ -36,44 +35,28 @@
   * error ... state will be set to error, processing stops 
   
   It's maybe a good idea to save the respons body to a key associated
-  to the state key (done).
-  
-  TODO: detect missing values that should be measured again.
-  Solution: Missing or wrong values are detected at postscripts which
-  leads to `:Retry true`. "
+  to the state key (done)."
   [body task m]
   (µ/log ::dispatch :message "try to write response" :m m)
   (stmem/set-val (assoc :func :resp :value body))
   (if-let [err (:error body)]
     (stmem/set-state-error (assoc m :message err))
-    (let [
-          retry    (:Retry      body)
-          to-exch  (:ToExchange body)
-          ids      (:ids        body)
-          results  (:Result     body)
-          doc-path (:DocPath    task)
-          mp-id    (:MpName     task)]
-      
-      (if retry
-        (let [res (do-retry m)
-              m   (assoc m :func :state)]
-          (cond
-            (:error res) (stmem/set-state-error m)
-            (:ok    res) (stmem/set-state-ready m)))
-        (let [res-ids   (doc/renew m ids)
-              res-exch  (exch/to (exch/all m) to-exch)
-              res-doc   (doc/store-results m results doc-path)
-              m         (assoc m :func :state)]
-          (cond
-            (:error res-exch) (stmem/set-state-error (assoc m :message "error at exchange"))
-            (:error res-doc)  (stmem/set-state-error (assoc m :message "error at document"))
-            (and
-             (:ok res-ids)
-             (:ok res-exch)     
-             (:ok res-doc))  (if (exch/stop-if (exch/all m) task)
-                               (stmem/set-state-executed m)
-                               (stmem/set-state-ready m))
-            :unexpected (stmem/set-state-error (assoc m :message "unexpected response"))))))))
+    (let [res-retry (if (contains? body :Retry) (do-retry m) {:ok true})
+          res-exch  (exch/to (exch/all m) (:ToExchange body))
+          res-ids   (doc/renew m (:ids body))
+          res-doc   (doc/store-results m (:Result body) (:DocPath task))]
+      (cond
+        (:error res-retry) (stmem/set-state-error (assoc m :message "error at retry"))
+        (:error res-exch)  (stmem/set-state-error (assoc m :message "error at exchange"))
+        (:error res-doc)   (stmem/set-state-error (assoc m :message "error at document"))
+        (and
+         (:ok res-retry)
+         (:ok res-ids)
+         (:ok res-exch)
+         (:ok res-doc)) (if (exch/stop-if (exch/all m) task)
+                          (stmem/set-state-executed m)
+                          (stmem/set-state-ready m))
+        :unexpected (stmem/set-state-error (assoc m :message "unexpected response"))))))
 
 ;;------------------------------
 ;; check
@@ -89,4 +72,5 @@
         (µ/log ::check :error "request for failed" :m m ))            
       (µ/log ::check :error "body can not be parsed" :m m))
     (µ/log ::check :error "no status in header" :m m)))
-)
+
+(comment)
