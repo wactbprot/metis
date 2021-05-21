b425a18a9f7405d4f645728ba376f8dcc044bd2e
wactbprot <wactbprot@users.noreply.github.com>
2021-05-21 12:04:20 +0000

    deploy: c96e2fa4a9ef64f5fc7289366a08be246bbad7df

--- metis/scheduler/interface.clj
+++
@@ -1,7 +0,0 @@
-✔ (ns metis.scheduler.interface
-?   [:require [metis.scheduler.core :as core]])
-  
-~ (defn start [mp-id] (core/start-ctrl mp-id))
-  
-~ (defn stop [mp-id] (core/stop-ctrl mp-id))
-  
