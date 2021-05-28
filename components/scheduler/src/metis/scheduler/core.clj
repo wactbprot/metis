b9f53e8616e860cc4a14103531d229c0bcbe43e0
Thomas Bock <thomas.bock@ptb.de>
2021-05-28 10:10:01 +0200

    new, much simpler scheduler

--- components/scheduler/src/metis/scheduler/core.clj
+++ components/scheduler/src/metis/scheduler/core.clj
@@ -14,9 +14,10 @@
   (stmem/get-maps (assoc m :func :state :seq-idx :* :par-idx :*)))
 
 (defn state
-  "`state` `proc`esses  on the state vector in order to find out whats next todo. The input `v`ector
-  looks like this:
+  "`state` `proc`esses on the state vector in order to find out whats
+  next todo. The input `v`ector looks like this:
 
+  Example:
   ```clojure
   [{:mp-id \"mpd-ref\",
   :struct :cont,
@@ -24,22 +25,15 @@
   :func :state,
   :seq-idx 0,
   :par-idx 0,
-  :value \"ready\"}
- {:mp-id \"mpd-ref\",
-  :struct :cont,
-  :no-idx 0,
-  :func :state,
-  :seq-idx 0,
-  :par-idx 1,
-  :value \"ready\"}
+  :value \"ready\"}]
   ```"
   [v]
   (let [m (proc/next-map v)]
     (cond
-      (proc/errors? v) {:state :error :m (first v)}
+      (proc/errors? v)       {:state :error    :m (first v)}
       (proc/all-executed? v) {:state :all-exec :m (first v)}
-      (nil? m) {:state :nop :m (first v)}
-      :else {:state :work :m m})))
+      (nil? m)               {:state :nop      :m (first v)}
+      :else                  {:state :work     :m m})))
 
 ;;------------------------------
 ;; state of ctrl interface 
@@ -51,43 +45,41 @@
 ;; check
 ;;------------------------------
 (defn check
-  "Processes whats todo next depending on the state of state and ctrl interface."
+  "Processes whats todo next depending on the state of state and ctrl
+  interface."
   [m]
-     
   (let [s (merge (state (state-interface m)) (ctrl m))
         m (:m s)]
-   
     (when-not (= (:ctrl s) :check)
-      (prn  s)
-      (Thread/sleep 1000)
-    
-      (stmem/set-ctrl (assoc m :value :check))
       (condp = (dissoc s :m)
         {:ctrl :run    :state :error}    (stmem/set-ctrl (assoc m :value :error))
         {:ctrl :mon    :state :error}    (stmem/set-ctrl (assoc m :value :error)) 
         {:ctrl :stop   :state :error}    (stmem/set-ctrl (assoc m :value :error))
         {:ctrl :run    :state :all-exec} (do
+                                           (stmem/set-ctrl   (assoc m :value :check))
                                            (stmem/set-states (assoc m :value :ready))
-                                           (stmem/set-ctrl (assoc m :value :ready)))
+                                           (stmem/set-ctrl   (assoc m :value :ready)))
         {:ctrl :mon    :state :all-exec} (do
+                                           (stmem/set-ctrl   (assoc m :value :check))
                                            (stmem/set-states (assoc m :value :ready))
-                                           (stmem/set-ctrl (assoc m :value :mon)))
+                                           (stmem/set-ctrl   (assoc m :value :mon)))
         {:ctrl :stop   :state :all-exec} (do
+                                           (stmem/set-ctrl   (assoc m :value :check))
                                            (stmem/set-states (assoc m :value :ready))
-                                           (stmem/set-ctrl (assoc m :value :ready)))
+                                           (stmem/set-ctrl   (assoc m :value :ready)))
         {:ctrl :reset  :state :nop}      (do
+                                           (stmem/set-ctrl   (assoc m :value :check))
                                            (stmem/set-states (assoc m :value :ready))
-                                           (stmem/set-ctrl (assoc m :value :ready)))
+                                           (stmem/set-ctrl   (assoc m :value :ready)))
         {:ctrl :stop   :state :nop}      (do
+                                           (stmem/set-ctrl   (assoc m :value :check))
                                            (stmem/set-states (assoc m :value :ready))
-                                           (stmem/set-ctrl (assoc m :value :ready)))
-        {:ctrl :run    :state :work}     (do
-                                           (worker/run m)
-                                           (stmem/set-ctrl (assoc m :value :run)))
-        {:ctrl :mon    :state :work}     (do
-                                           (worker/run m)
-                                           (stmem/set-ctrl (assoc m :value :run)))
-        (mu/log ::dispatch :message "what todo with state" :state s)))))
+                                           (stmem/set-ctrl   (assoc m :value :ready)))
+        {:ctrl :run    :state :work}       (worker/run m)
+        {:ctrl :mon    :state :work}       (worker/run m)
+                                           
+        
+        (mu/log ::dispatch :message "state not handeled" :state s)))))
 
 ;;------------------------------
 ;; ctrl interface
