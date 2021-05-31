3ea8cfd884102a87421d905feae9f1669e80d28b
Thomas Bock <thomas.bock@ptb.de>
2021-05-31 15:00:58 +0200

    doc interface with conf arity

--- components/document/src/metis/document/interface.clj
+++ components/document/src/metis/document/interface.clj
@@ -1,12 +1,28 @@
 (ns metis.document.interface
   (:require [metis.document.core :as core]))
 
-(defn add [m id] (core/add m id))
+(defn add
+  ([m id]
+   (core/add m id))
+  ([conf m id]
+   (core/add conf m id)))
 
-(defn rm [m id] (core/rm m id))
+(defn rm
+  ([m id]
+   (core/rm m id))
+  ([conf m id]
+   (core/rm conf m id)))
 
-(defn store-results [m results doc-path] (core/store-results m results doc-path)) 
+(defn store-results
+  ([m results doc-path]
+   (core/store-results m results doc-path)) 
+  ([conf m results doc-path]
+   (core/store-results conf m results doc-path)))
 
-(defn renew [m v] (core/renew m v))
+(defn renew
+  ([m v]
+   (core/renew m v))
+  ([conf m v]
+   (core/renew conf m v)))
 
 (defn ids [m] (core/ids m))
