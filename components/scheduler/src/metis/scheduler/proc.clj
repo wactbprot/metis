4c88a7cde2fa4b26bbe1157eecd65d62338a92cc
Wact. B. Prot <wactbprot@gmail.com>
2021-06-23 19:49:45 +0200

    reduce scheduler proc

--- components/scheduler/src/metis/scheduler/proc.clj
+++ components/scheduler/src/metis/scheduler/proc.clj
@@ -1,30 +1,29 @@
 (ns metis.scheduler.proc
   ^{:author "wactbprot"
     :doc "Finds and starts the up comming tasks of a certain container."}
-  (:require [com.brunobonacci.mulog :as mu]
-            ))
+  (:require [com.brunobonacci.mulog :as mu]))
   
-(defn filter-state
+(defn filter-value
   "Returns a vector of maps where `:value` is `s`."
-  [v s]
-  (filterv (fn [m] (= s (:value m))) v))
+  [v kw]
+  (filterv #(= kw (keyword (:value %))) v))
 
 (defn filter-seq-idx
   "Returns a vector of maps where `:seq-idx` is `i`."
   [v i]
-  (filterv (fn [m] (= i (:seq-idx m))) v))
+  (filterv #(= i (:seq-idx %)) v))
 
 (defn all-error
   "Returns all steps with the state `:error` for a given state vector
   `v`"
   [v]
-  (filter-state v "error"))
+  (filter-value v :error))
 
 (defn all-ready
   "Returns all steps with the state `:ready` for a given state vector
   `v`"
   [v]
-  (filter-state v "ready"))
+  (filter-value v :ready))
 
 (defn all-executed
   "Returns all-executed entrys of the given vector `v`.
@@ -39,7 +38,7 @@
   ;; [{:seq-idx 0 :par-idx 0 :state :executed}]
   ```"
   [v]
-  (filter-state v "executed"))
+  (filter-value v :executed))
 
 (defn all-executed?
   "Checks if all entries of map `m` are executed"
@@ -61,12 +60,7 @@
   "Checks if the steps of `v` before `i` are `all-executed?`. Returns
   `true` for `i` equal to zero (or negative `i`)"
   [v i]
-  (if (pos? i)
-    (every? true? (map
-                   (fn [j] (all-executed? (filter-seq-idx v j)))
-                   (range i)))
-      true))
-
+  (every? true? (map #(all-executed? (filter-seq-idx v %)) (range i))))
 
 ;;------------------------------
 ;; choose and start next task
@@ -88,7 +82,6 @@
   ;; {:seq-idx 3 :par-idx 1 :state :ready}
   ```"
   [v]
-  (when-let [next-m (next-ready v)]
-    (when-let [i (:seq-idx next-m)]
-      (when (or (zero? i) (predecessors-executed? v i))
-        next-m))))
+  (when-let [m (next-ready v)]
+    (when-let [i (:seq-idx m)]
+      (when (or (zero? i) (predecessors-executed? v i)) m))))
