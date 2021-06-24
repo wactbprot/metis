ea908e239698e5feb6f4a36ed9ff7f0bf299162b
Thomas Bock <thomas.bock@ptb.de>
2021-06-24 10:59:29 +0200

    working websocket updates

--- bases/srv/resources/public/js/ws.js
+++ bases/srv/resources/public/js/ws.js
@@ -1,10 +1,36 @@
 var ws = new WebSocket("ws://127.0.0.1:8010/ws")
 
+const mp_id = $("#body").data("mp-id"); 
+
+const sep = "_";
+
+
+var gen_state_id = (data) => {
+    return [mp_id, data["struct"], data["no-idx"], "state", 
+	    data["seq-idx"], data["par-idx"]].join(sep)
+}
+
+var gen_ctrl_id = (data) => {
+    return [mp_id, data["struct"], data["no-idx"],"ctrl"].join(sep)
+}
+
+
 ws.onopen = function (event) {
    ws.send(JSON.stringify({"ok":true}));
 };
 
+
 ws.onmessage = function (event) {
     var data =JSON.parse(event.data);
-    console.log(data);
+
+    if(data["mp-id"] == mp_id){
+	if(data["func"] == "state"){
+	    var id = gen_state_id(data);
+	    $("#" + id).html(data["value"]);
+	}
+	if(data["func"] == "ctrl"){
+	    var id = gen_ctrl_id(data);
+	    $("#" + id).html(data["value"]);
+	}
+    }
 }
