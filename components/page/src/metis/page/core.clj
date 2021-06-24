56d97aff2fe24ad80edd55ca91d6de8066bc3d4c
Thomas Bock <thomas.bock@ptb.de>
2021-06-24 10:59:29 +0200

    working websocket updates

--- components/page/src/metis/page/core.clj
+++ components/page/src/metis/page/core.clj
@@ -1,11 +1,12 @@
 (ns metis.page.core
   (:require [hiccup.form :as hf]
             [hiccup.page :as hp]
+            [metis.page.utils :as u]
             [clojure.string :as string]))
 
 (defn table-row [m]
   [:tr
-   [:td (:value m)]
+   [:td {:id (u/gen-state-id m)} (:value m)]
    [:td [:button.uk-button.uk-button-default [:span {:uk-icon "icon: cog" :title "working"}]]
     [:button.uk-button.uk-button-default [:span {:uk-icon "icon: check" :title "executed"}]]
     [:button.uk-button.uk-button-default [:span {:uk-icon "icon: play" :title "ready"}]]]
@@ -23,7 +24,7 @@
          [:button.uk-button.uk-button-default "suspend"] 
          [:table.uk-table.uk-table-hover.uk-table-striped
           [:thead [:tr
-                   [:th "status"]
+                   [:th.uk-width-small "status"]
                    [:th "ctrls"]
                    [:th "seq-idx"]
                    [:th "par-idx"]
@@ -34,7 +35,7 @@
 
 (defn ctrl-li-title [m]
   [:a.uk-accordion-title {:href "#"}
-   [:span.uk-text-light (:no-idx m) " / "(:value m)]
+   [:span.uk-text-light (:no-idx m) " / "] [:span.uk-text-light.uk-width-expand {:id (u/gen-ctrl-id m)} (:value m)]
    [:span.uk-align-right (:descr m)]])
 
 (defn ctrl-li [m] [:li (ctrl-li-title m) (state-li (:states m))])
@@ -61,11 +62,11 @@
       [:li [:a {:href ""} "devproxy"]]]]]])
 
 (defn body [conf data]
-  [:body  (nav conf)
+  [:body#body {:data-mp-id (:mp-id data)} (nav conf)
    (content conf data)
-   (hp/include-js "/js/uikit.min.js")
-   (hp/include-js "/js/ws.js")
    (hp/include-js "/js/jquery-3.5.1.min.js")
-   (hp/include-js "/js/uikit-icons.min.js")])
+   (hp/include-js "/js/uikit.min.js")
+   (hp/include-js "/js/uikit-icons.min.js")
+   (hp/include-js "/js/ws.js")])
 
 (defn index [conf data] (hp/html5 (head conf data) (body conf data)))
--- components/page/src/metis/page/utils.clj
+++ components/page/src/metis/page/utils.clj
@@ -1,2 +1,11 @@
 (ns metis.page.utils
   (:require [clojure.string :as string]))
+
+  (def s "_")
+
+(defn gen-state-id [{a :mp-id b :struct c :no-idx  d :seq-idx e :par-idx}]
+  (string/join s [a (name b) c "state" d e ]))
+
+(defn gen-ctrl-id [{a :mp-id b :struct c :no-idx}]
+  (string/join s [a (name b) c "ctrl"]))
+
