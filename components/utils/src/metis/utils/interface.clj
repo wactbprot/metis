e9ac0be64ddbb51cb622f01feae84b188ae07d58
Thomas Bock <thomas.bock@ptb.de>
2021-07-19 10:40:52 +0200

    safe map fn to model ns

--- components/utils/src/metis/utils/interface.clj
+++ components/utils/src/metis/utils/interface.clj
@@ -9,8 +9,6 @@
 
 (defn ensure-int [i] (core/ensure-int i))
 
-(defn map->safe-map [m] (core/map->safe-map m))
-
 (defn short-string [s] (core/short-string s))
 
 (defn apply-to-map-values [f m] (core/apply-to-map-values f m)) 
