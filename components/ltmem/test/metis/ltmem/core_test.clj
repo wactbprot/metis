51f610c5a6568e7a7206018d3ecbc74744edddf9
Thomas Bock <thomas.bock@ptb.de>
2021-08-09 07:50:22 +0200

    safe -> ltmem

--- components/ltmem/test/metis/ltmem/core_test.clj
+++ components/ltmem/test/metis/ltmem/core_test.clj
@@ -16,3 +16,20 @@
   (testing "generation"
     (is (= (:ltmem-db conf) (:db_name (couch/get-database (:ltmem-conn conf))))
         "db or exist (needs env var DB_ADMIN and DB_PWD)")))
+
+(deftest safe-map-i
+  (testing "nil behaviour"
+    (is (nil? (safe nil))))
+  (testing "empty"
+    (is (empty? (safe {}))))
+  (testing "simple keep"
+    (is (= {:a "@123"}  (safe {:a "@123"}))))
+  (testing "simple change"
+    (is (= {:a "%foo"}  (safe {:a "@foo"}))))
+  (testing "cuts a string (i)"
+    (is (= {:a "(@101,102)"} (safe {:a "(@101,102)"})) 
+        "conserves @ infront of numbers")
+    (is (= {:a "foo (@101,102) bar"} (safe {:a "foo (@101,102) bar"})) 
+        "conserves @ infront of numbers")
+    (is (= {:a "%foo (@101,102)%bar"} (safe {:a "@foo (@101,102)%bar"})) 
+        "conserves @ infront of numbers")))
--- components/model/test/metis/model/core_test.clj
+++ components/model/test/metis/model/core_test.clj
@@ -2,20 +2,3 @@
   (:require [clojure.test :refer :all]
             [metis.model.core :refer :all]))
 
-
-(deftest safe-map-i
-  (testing "nil behaviour"
-    (is (nil? (safe nil))))
-  (testing "empty"
-    (is (empty? (safe {}))))
-  (testing "simple keep"
-    (is (= {:a "@123"}  (safe {:a "@123"}))))
-  (testing "simple change"
-    (is (= {:a "%foo"}  (safe {:a "@foo"}))))
-  (testing "cuts a string (i)"
-    (is (= {:a "(@101,102)"} (safe {:a "(@101,102)"})) 
-        "conserves @ infront of numbers")
-    (is (= {:a "foo (@101,102) bar"} (safe {:a "foo (@101,102) bar"})) 
-        "conserves @ infront of numbers")
-    (is (= {:a "%foo (@101,102)%bar"} (safe {:a "@foo (@101,102)%bar"})) 
-        "conserves @ infront of numbers")))
