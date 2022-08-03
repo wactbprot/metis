16860012c0bdcad2281eb774a3021498ce4177e3
Thomas Bock <thomas.bock@ptb.de>
2022-08-03 16:36:57 +0200

    eod

--- components/page/src/metis/page/mpd_ppc_gas_dosing.clj
+++ components/page/src/metis/page/mpd_ppc_gas_dosing.clj
@@ -7,7 +7,8 @@
 (def mp-id "mpd-ppc-gas_dosing")
 
 (def target-pressure-opts 
-  [{:value 0.1 :display "0.1"}
+  [{:value 0 :display "0"}
+   {:value 0.1 :display "0.1"}
    {:value 0.2 :display "0.2"}
    {:value 1 :display "1"}
    {:value 2 :display "2"}
@@ -21,10 +22,9 @@
   ([mpd exchpath exchkey] (str mpd "_exch_" exchpath "_" exchkey)))
 
 (defn input
-  ([exchpath label all-exch]
-   (input exchpath "Value" label all-exch))
+  ([exchpath         label all-exch] (input exchpath "Value" label all-exch))
   ([exchpath exchkey label all-exch]
-  (let [id (id-> exchpath exchkey)]
+   (let [id (id-> exchpath exchkey)]
     [:div
      [:label.uk-form-label {:for id} label]
      [:div.uk-form-controls
@@ -32,8 +32,8 @@
        {:type "text" :id id :value (get-in all-exch [exchpath (keyword exchkey)])}]]])))
 
 (defn select [exchpath opts all-exch]
-  (let [x           (get-in all-exch [exchpath :Selected])
-        default-opt [:option {:value x} x]
+  (let [selected-val (get-in all-exch [exchpath :Selected])
+        default-opt [:option {:value selected-val} selected-val]
         select-map  {:data-mp-id  mp-id
                      :data-struct "exch"
                      :data-exchpath exchpath}]
@@ -47,13 +47,53 @@
    
    [:div {:uk-grid ""}
     
+    ;; ctrl container
+    [:button.uk-button.uk-button-default.uk-button-danger.ctrl-btn
+     {:data-mp-id mp-id
+      :data-struct "cont"
+      :data-no-idx "0"
+      :data-func "ctrl"
+      :data-value "run"} "Initialize Devices"]
+    
+    [:button.uk-button.uk-button-default.ctrl-btn
+     {:data-mp-id mp-id
+      :data-struct "cont"
+      :data-no-idx "1"
+      :data-func "ctrl"
+      :data-value "run"} "Open TMP Valve"]
+
+    [:button.uk-button.uk-button-default.ctrl-btn
+     {:data-mp-id mp-id
+      :data-struct "cont"
+      :data-no-idx "2"
+      :data-func "ctrl"
+      :data-value "run"} "Close TMP Valve"]
+
+    [:button.uk-button.uk-button-default.uk-button-primary.ctrl-btn
+     {:data-mp-id mp-id
+      :data-struct "cont"
+      :data-no-idx "3"
+      :data-func "ctrl"
+      :data-value "mon"} "Start Gas Dosing"]
+    
+    [:button.uk-button.uk-button-default.uk-button-secondary.ctrl-btn
+     {:data-mp-id mp-id
+      :data-struct "cont"
+      :data-no-idx "3"
+      :data-func "ctrl"
+      :data-value "stop"} "Stop Gas Dosing"]]
+
+   [:div {:uk-grid ""}
+    
     ;; obs pressure
     [:div.uk-card.uk-card-body.uk-card-default
      (input "ObservePressure" "Value" "Observer Pressure (DualGauge, mbar)" all-exch)]
     [:div.uk-card.uk-card-body.uk-card-default
+     (input "Servo_PPC_Pos" "Value" "Position of TMP Valve" all-exch)]
+    [:div.uk-card.uk-card-body.uk-card-default
      (input "PPCVATDosingValve" "Mode" "VAT-Valve Operation Mode" all-exch)]
     [:div.uk-card.uk-card-body.uk-card-default
-     (input "Servo_PPC_Pos" "Value" "Position of TMP" all-exch)]]
+     (input "PPCVATDosingValve" "Position" "Position of VAT-Valve" all-exch)]]
    
    [:div {:uk-grid ""}
 
